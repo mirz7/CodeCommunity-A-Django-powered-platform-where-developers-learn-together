5f2e2e276f0ae277fbc62893814eb18f0906072c
mirz7 <mirzzpc1289@gmail.com>
2025-11-11 23:24:15 +0530

    New insertions

--- studysumu/base/views.py
+++ studysumu/base/views.py
@@ -64,7 +64,7 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains=q)|
                                 Q(name__icontains=q) |
                                 Q(description__icontains=q) )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.all().filter(Q(room__topic__name__icontains=q))
     context = {'rooms':rooms,'topics':topics ,'room_count':room_count,'room_messages':room_messages}
@@ -172,3 +172,14 @@ def updateUser(request):
             form.save()
             return redirect('user-profile',pk=user.id)
     return render(request,'base/update-user.html',{'form':form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics':topics}
+    return render(request,'base/topics.html',context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages':room_messages}
+    return render(request,'base/activity.html',context)
